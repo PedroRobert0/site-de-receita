6bd748185af7ca157f975390846b72634912af49
ChefAIofc <chefai.ofc@gmail.com>
2025-06-02 21:28:04 -0300

    Primeiro commit - estrutura inicial do ChefAI

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,10 @@
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+    return (
+    <main>
+        <h1>Bem-vindo ao ChefAI!</h1>
+        <p>Seu site de receitas com inteligência artificial.</p>
+    </main>
+    );
+}
